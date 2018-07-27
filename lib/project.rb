ffd48daf8b6fa05c43c5fe5cae6b9178a6d68d4d
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-07-27 14:23:03 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,9 @@
 class Project
   attr_accessor :title, :backers
-  #@@all = []
 
   def initialize(title)
     @title = title
     @backers = []
-     #@@all << self
   end
 
   def add_backer(backer)
